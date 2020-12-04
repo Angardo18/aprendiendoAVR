e8736c6182d409123143994a94e13bf39b0fab52
Angardo18 <angelos180900@gmail.com>
2020-12-04 11:43:01 -0600

    usando el tmr0

---
+++ controlTM1637/controlTM1637/main.asm
@@ -0,0 +1,12 @@
+;
+; controlTM1637.asm
+;
+; Created: 4/12/2020 11:11:35
+; Author : Angel Orellana
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
---
+++ timer0/timer0/main.asm
@@ -0,0 +1,12 @@
+;
+; timer0.asm
+;
+; Created: 4/12/2020 11:13:40
+; Author : Angel Orellana
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
